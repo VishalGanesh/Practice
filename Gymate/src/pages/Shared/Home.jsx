fd8dcdeb507d9334497fe0cc3578504d81bc0614
VishalGanesh <kingv4551@gmail.com>
2024-07-24 22:27:17 +0530

    Login/Signup page
    #loadingpage #theme

---
+++ Gymate/src/pages/Shared/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        // <nav className='navbar'>
+        // <div className="navbar-container"></div>
+        <div className='h-full w-full flex justify-center items-center'>        
+        <div className="navbar-logo">BLAZE TO VICTORY</div>            
+        {/* <button className="navbar-button">Click me</button> */}
+        </div>
+        
+    )
+}
+
+export default Home
